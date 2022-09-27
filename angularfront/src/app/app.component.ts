497127d77ad842fb5b846b1e6b99b62634cd348a
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-09-27 14:01:36 +0300

    front and back testing

--- angularfront/src/app/app.component.ts
+++ angularfront/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor() {}
-  showAlert() {}
+  users: any[];
+  constructor(private http: HttpClient) {}
+
+  fetchData() {
+    this.http.get('http://localhost:8080/api/users').subscribe((data: any) => {
+      this.users = data;
+      console.log(data);
+    });
+  }
 }
