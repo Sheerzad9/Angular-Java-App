2e7f7b2eff2503ff169c5f3d474557c6dd430ae2
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-10-04 18:54:40 +0300

    Added homepage template

---
+++ angularfront/src/app/homepage/homepage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+})
+export class HomepageComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  logOut() {
+    this.authService.logout();
+  }
+}
