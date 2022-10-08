df63ee4ec92f58cc801692833e357b5cbda80cd4
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-10-08 14:58:41 +0300

    Added menu component

---
+++ angularfront/src/app/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  doLogOut() {
+    this.authService.logout();
+  }
+}
