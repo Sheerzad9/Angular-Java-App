407fb1f3ab0ed39f880f5fbb161dedc1dcf117bf
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-03-04 12:01:06 +0200

    new logic

--- angularfront/src/app/profilepage/profilepage.component.ts
+++ angularfront/src/app/profilepage/profilepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PostFeed } from '../post/post.component';
 import { ProfileService } from './profile.service';
 
 @Component({
@@ -8,22 +9,28 @@ import { ProfileService } from './profile.service';
   styleUrls: ['./profilepage.component.css'],
 })
 export class ProfilepageComponent implements OnInit {
+  id: number;
+  postFeed: any;
   constructor(
     private activedRoute: ActivatedRoute,
     private profileService: ProfileService
-  ) {}
-
-  ngOnInit(): void {
-    let id;
+  ) {
     if (this.activedRoute.snapshot.queryParams['id']) {
       this.activedRoute.queryParams.subscribe((params) => {
-        id = params.id;
+        this.id = params.id;
         console.log(params);
       });
     }
-    this.profileService.getUserInfo(id).subscribe(
+  }
+
+  ngOnInit(): void {
+    this.getPostfeed();
+  }
+  getPostfeed() {
+    this.profileService.getUserInfo(this.id).subscribe(
       (res) => {
         console.log(res);
+        this.postFeed = res;
       },
       (err) => console.log(err)
     );
