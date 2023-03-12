165360beaef810d6ac80df5ef7e96c8cfc1ccf64
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-03-12 18:28:10 +0200

    Updated sppinner and positioned it to center

--- angularfront/src/app/shared/spinner/spinner.component.ts
+++ angularfront/src/app/shared/spinner/spinner.component.ts
@@ -4,7 +4,7 @@ import { LoadingService } from '../loading.service';
 @Component({
   selector: 'spinner',
   template:
-    "<ng-container *ngIf='loadingService.isLoading | async'><div class='center'><div class='spinner-border text-info' style='width: 10rem; height: 10rem; margin-top: 50%;' role='status'><span class='sr-only'>Loading...</span></div></div></ng-container>",
+    "<ng-container *ngIf='loadingService.isLoading | async'><div class='loader'><div class='loader-inner'><div class='lds-ring'><div></div><div></div><div></div><div></div></div></div></div></ng-container>",
   styleUrls: ['./spinner.component.css'],
 })
 export class SpinnerComponent {
